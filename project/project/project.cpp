0398d0418e857a67dfdd17a2d91ec7bad488ebf0
Alice(Karine) <karine@example.com>
2023-06-30 11:22:31 +0300

    code: some changes

--- project/project/project.cpp
+++ project/project/project.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
-int main() {
-	std::cout << "Hello World";
+int main()
+{
+	std::cout << "Enter A and B: ";
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << "A + B = " << a + b << '\n';
 }
